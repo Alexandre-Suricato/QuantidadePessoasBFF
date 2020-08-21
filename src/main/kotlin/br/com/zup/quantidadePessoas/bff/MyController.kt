03343f36b6160a323d92ae15b299d9c9c02db886
suricatoti <alexandredslima@gmail.com>
2020-08-21 15:43:16 -0300

    Images inserctions

--- src/main/kotlin/br/com/zup/quantidadePessoas/bff/MyController.kt
+++ src/main/kotlin/br/com/zup/quantidadePessoas/bff/MyController.kt
@@ -5,15 +5,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MyController(private val myService: MyService) {
-    @GetMapping("/action")
-    fun getAction() = myService.createAction()
 
     @GetMapping("/screen")
     fun getScreen() = myService.createScreen()
 
-    @GetMapping("/builder")
-    fun getScreenBuilder() = myService.createScreenBuilder()
-
-    @GetMapping("/widget")
-    fun getWidget() = myService.createWidget()
 }
