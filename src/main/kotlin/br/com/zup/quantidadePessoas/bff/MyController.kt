52708084c26e6af6cb492186e8e816f3c644a986
suricatoti <alexandredslima@gmail.com>
2020-08-21 13:41:02 -0300

    First Commit

---
+++ src/main/kotlin/br/com/zup/quantidadePessoas/bff/MyController.kt
@@ -0,0 +1,19 @@
+package br.com.zup.quantidadePessoas.bff
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MyController(private val myService: MyService) {
+    @GetMapping("/action")
+    fun getAction() = myService.createAction()
+
+    @GetMapping("/screen")
+    fun getScreen() = myService.createScreen()
+
+    @GetMapping("/builder")
+    fun getScreenBuilder() = myService.createScreenBuilder()
+
+    @GetMapping("/widget")
+    fun getWidget() = myService.createWidget()
+}
